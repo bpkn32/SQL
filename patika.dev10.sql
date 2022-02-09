19bf99225e4f3de8f6beda5e36e52030cb6e3eba
Berk Pişkin <berkpiskin.a@gmail.com>
2022-02-09 21:40:15 +0300

    Update patika.dev10.sql

--- patika.dev10.sql
+++ patika.dev10.sql
@@ -1,8 +1,14 @@
+-- 1. Soru :
+
 SELECT city,country FROM city
 LEFT JOIN country ON city.country_id = country.country_id;
 
+-- 2. Soru :
+
 SELECT payment_id,first_name,last_name FROM payment
 RIGHT JOIN customer ON customer.customer_id = payment.customer_id;
+
+-- 3. Soru :
 
 SELECT rental_id,first_name,last_name FROM customer
 FULL JOIN rental ON rental.customer_id = customer.customer_id;
